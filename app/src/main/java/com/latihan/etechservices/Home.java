f0a55976d42ac71bc96aa7137df9eabc948a4713
Dendi Fazar Zaman <dendi.fazarzaman.tik19@mhsw.pnj.ac.id>
2021-05-29 12:01:15 +0700

    Adding Home + Landpage

---
+++ app/src/main/java/com/latihan/etechservices/Home.java
@@ -0,0 +1,26 @@
+package com.latihan.etechservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /** Hiding Title bar of this activity screen */
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        /** Making this activity, full screen */
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        /** Hide Action Bar Of This Activity Screen */
+        getSupportActionBar().hide();
+        setContentView(R.layout.home);
+        Typeface type = Typeface.createFromAsset(getAssets(), "font/coolvetica.ttf");
+
+    }
+}
